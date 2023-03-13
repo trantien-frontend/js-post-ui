bc52e879d0085b84ff9601347424da50fd5dc4ad
Tran Tien <tientran0963598@gmail.com>
2023-03-13 07:01:02 +0700

    new-file

--- js/utils/post-form.js
+++ js/utils/post-form.js
@@ -1,4 +1,5 @@
-import { setFiledValue, setImageUrl } from './common';
+import { setFiledValue, setImageUrl, setTextContent } from './common';
+import * as yup from 'yup';
 
 function setValueInputForm(formElement, value) {
   if (!value) return;
@@ -20,6 +21,83 @@ function getFormsValues(formElement) {
 
   return formValues;
 }
+function getPostSchema() {
+  return yup.object().shape({
+    title: yup.string().required('please enter title'),
+    author: yup
+      .string()
+      .required('please enter author')
+      .test('at-least-to-words', 'at least to words', (value) => {
+        return value.split(' ').filter((x) => !!x && x.length > 2).length >= 2;
+      }),
+    description: yup.string(),
+  });
+}
+
+// function getTitleError(formElement) {
+//   const inputTitle = formElement.querySelector('[name="title"]');
+//   if (!inputTitle) return;
+
+//   //   requied
+//   if (inputTitle.validity.valueMissing) return 'case requied';
+//   // has 3 words and least word has 3 character
+//   if (inputTitle.value.split(' ').filter((x) => !!x && x.length > 2).length < 2)
+//     return 'case words';
+
+//   return '';
+// }
+function setFieldError(form, name, errorMessage) {
+  const inputField = form.querySelector(`[name="${name}"]`);
+  if (inputField) {
+    inputField.setCustomValidity(errorMessage);
+    setTextContent(inputField.parentElement, '.invalid-feedback', errorMessage);
+  }
+}
+
+async function validatePostForm(formElement, formValues) {
+  if (!formElement) return;
+
+  // const listErrorMess = {
+  //   title: getTitleError(formElement),
+  // };
+
+  // for (const errorMess in listErrorMess) {
+  //   const inputTitleElement = formElement.querySelector(`[name=${errorMess}]`);
+  //   if (inputTitleElement) {
+  //     inputTitleElement.setCustomValidity(listErrorMess[errorMess]);
+  //     setTextContent(
+  //       inputTitleElement.parentElement,
+  //       '.invalid-feedback',
+  //       listErrorMess[errorMess]
+  //     );
+  //   }
+  // }
+
+  try {
+    // reset form
+    const fieldList = ['title', 'author'];
+    fieldList.forEach((field) => setFieldError(formElement, field, ''));
+
+    const schema = getPostSchema();
+    await schema.validate(formValues, { abortEarly: false });
+  } catch (error) {
+    const errorLog = {};
+    if (error.name === 'ValidationError' || Array.isArray(error.inner)) {
+      for (const validationError of error.inner) {
+        const name = validationError.path;
+
+        if (errorLog[name]) continue;
+
+        setFieldError(formElement, name, validationError.message);
+        errorLog[name] = true;
+      }
+    }
+  }
+
+  const isValid = formElement.checkValidity();
+  if (!isValid) formElement.classList.add('was-validated');
+  return isValid;
+}
 
 export function initPostForm({ idElementForm, defaultValue, onSubmit }) {
   const form = document.getElementById(idElementForm);
@@ -31,6 +109,10 @@ export function initPostForm({ idElementForm, defaultValue, onSubmit }) {
     event.preventDefault();
 
     const formValues = getFormsValues(form);
+
+    if (!validatePostForm(form, formValues)) return;
+
+    console.log('submited form');
   });
   //   onSubmit(defaultValue);
 }
--- vite.config.js
+++ vite.config.js
@@ -1,13 +1,13 @@
-import { resolve } from "path";
-import { defineConfig } from "vite";
+import { resolve } from 'path';
+import { defineConfig } from 'vite';
 
 export default defineConfig({
   build: {
     rollupOptions: {
       input: {
-        main: resolve(__dirname, "index.html"),
-        nested: resolve(__dirname, "post-detail.html"),
-        nested: resolve(__dirname, "add-edit-post.html"),
+        main: resolve(__dirname, 'index.html'),
+        nested: resolve(__dirname, 'post-detail.html'),
+        nested: resolve(__dirname, 'add-edit-post.html'),
       },
     },
   },
